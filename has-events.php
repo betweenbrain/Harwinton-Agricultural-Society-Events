75b2f7a11f2cfcabddb6064d8287e4519b4ffd4f
Matt Thomas <mthomas@guggenheim.org>
2019-12-15 19:05:26 -0500

    Working POC of using a Google Map to set latLng field data

--- has-events.php
+++ has-events.php
@@ -8,8 +8,8 @@
  */
 
  /**
- * Register activity custom post type.
- */
+  * Register activity custom post type.
+  */
 add_action(
 	'init', function () {
 		$args = array(
@@ -118,18 +118,111 @@ add_action(
 );
 
 /**
- * hasEvents Google Maps API Key:
- * AIzaSyAMTbtiwfB3H78Es2uoY1O93_3LsqzkgIc
+ * Render field when creating and editing taxonomy.
+ *
+ * https://developer.wordpress.org/reference/hooks/taxonomy_add_form_fields/
  */
-/*
-function acf_init() {
-    acf_update_setting('google_api_key', 'AIzaSyAMTbtiwfB3H78Es2uoY1O93_3LsqzkgIc');
-}
-add_action('acf/init', 'acf_init');
-*/
+add_action( 'location_add_form_fields', 'render_map', 10, 1 );
+add_action( 'location_edit_form_fields', 'render_map', 10, 1 );
 
-function my_acf_google_map_api( $api ){
-    $api['key'] = 'AIzaSyAMTbtiwfB3H78Es2uoY1O93_3LsqzkgIc';
-    return $api;
+function render_map( $term ) {
+	?>
+	<div class="form-field term-location-wrap">
+	<div id="map"></div>
+		<label for="latLng">Location</label>
+		<input type="hidden" name="latLng" id="latLng" value="<?php echo property_exists( $term, 'term_id' ) ? get_term_meta( $term->term_id, 'latLng', true ) : ''; ?>" >
+		<p>Click the map to set the exact location.</p>
+	</div>
+	<?php
 }
-add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
+
+add_action( 'location_edit_form', 'add_script', 10, 1 );
+add_action( 'location_add_form', 'add_script', 10, 1 );
+
+function add_script( $term ) {
+	$latLng = property_exists( $term, 'term_id' ) ? get_term_meta( $term->term_id, 'latLng', true ) : '';
+	?>
+	<style>
+		#map {
+			height: 400px;
+			width: 400px
+		}
+	</style>
+	<script>
+		const input = document.getElementById('latLng');
+		const latLng = '<?php echo $latLng; ?>';
+
+		function initMap() {
+			const map = new google.maps.Map(document.getElementById('map'), {
+				center: { lat: 41.763031, lng: -73.044465 },
+				zoom: 17.75
+			});
+			// TODO: Refactor methods into one.
+
+			/** 
+			* Render saved marker
+			*/ 
+			if(latLng){
+				const cords = latLng.replace('(', '').replace(')', '').split(',');
+				const marker = new google.maps.Marker({
+					position: new google.maps.LatLng(cords[0], cords[1]),
+					map: map,
+				});
+
+				google.maps.event.addListener(marker, 'click', function () {
+					marker.setMap(null);
+					input.setAttribute('value', '');
+				});
+			}
+
+			google.maps.event.addListener(map, 'click', function (event) {
+				// TODO: Unset other markers.
+
+				/** 
+				* Add a new marker
+				*/
+				const marker = new google.maps.Marker({
+					position: event.latLng,
+					map: map,
+				});
+
+				// Update hidden field
+				input.setAttribute('value', event.latLng);
+
+				// Remove existing marker if clicked
+				google.maps.event.addListener(marker, 'click', function () {
+					marker.setMap(null);
+					input.setAttribute('value', '');
+				});
+			});
+		}
+	</script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=[API KEY GOES HERE]&callback=initMap"
+		async defer></script>
+
+	<?php
+}
+
+/**
+ * Save latLang when created
+ */
+add_action( 'created_location', 'save_latLang', 10, 1 );
+
+function save_latLang( $term_id ) {
+	if ( isset( $_POST['latLng'] ) ) {
+		add_term_meta( $term_id, 'latLng', $_POST['latLng'], true );
+	}
+}
+
+/**
+ * Save latLang when editing
+ */
+add_action( 'edited_location', 'update_latLng', 10, 1 );
+
+function update_latLng( $term_id ) {
+	if ( isset( $_POST['latLng'] ) ) {
+		update_term_meta( $term_id, 'latLng', $_POST['latLng'] );
+	} else {
+		update_term_meta( $term_id, 'latLng', '' );
+	}
+}
